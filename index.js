d28517160dd687d451525a66f15b985f249ab56f
sgimenoj <sgimenoj@ext.aragon.es>
2024-06-19 15:55:23 +0200

    .

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+let dns = require('dns')
+const validUrl = require("valid-url");
+let bodyParser = require("body-parser");
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -14,6 +17,45 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/", function (req, res, next) {
+  console.log(req.method + " " + req.url);
+  
+  if (req.method == "POST") {
+    console.log(req.body);
+  }
+  
+  next();
+});
+
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url;
+
+  console.log(url)
+
+  let t = await validateUrl(url);
+
+  if(t != "ok"){
+    res.json({
+      error: t
+    });
+    return
+  }
+
+  res.json({
+    original_url: url,
+    short_url: 1,
+  });
+})
+
+app.get("/api/shorturl/:url", (req, res) => {
+  console.log(req.params.url);
+  req.baseUrl = ""
+  res.redirect(302, "" + req.params.url);
+})
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
@@ -22,3 +64,25 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+async function validateUrl(url){
+
+  if(!url || url == ""){
+    return "empty url";
+  }
+
+  const hostname = new URL(url).hostname;
+
+  if (hostname === "localhost" || hostname === "127.0.0.1") {
+    return "ok"
+  }
+
+  try{
+    await dns.promises.lookup(url, {});
+  } catch(e){
+      return "invalid url";
+  }
+
+  return "ok"
+
+}
